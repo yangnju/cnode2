a0b5b50f59d0a9279666b417a6847d9189ebbbdd
yangnju <yangfeng611@gmail.com>
2021-02-04 22:30:51 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,31 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div class="postlist">
+    <p>这是helloworld组件</p>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li v-for="post in posts" :key="post.id">
+        <img :src="post.author.avatar_url">
+        <span class="count">
+          <span class="repay_count">{{ post.reply_count }}</span>/
+          <span class="visit_count">{{ post.visit_count }}</span>
+        </span>
+        <!-- 当post.good是真，元素类名是put_good -->
+        <span class="tab" :class="[{put_good:(post.good === true),put_top:(post.top === true),topic_tab:(post.good !== true && post.top !== true)}]">
+          <span>{{post | tabFormatter}}</span>
+        </span>
+        <span class="title">{{ post.title }}</span>
+        <span class="last_reply">{{ post.last_reply_at }}</span>
+      </li>
     </ul>
   </div>
 </template>
@@ -33,26 +22,40 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      posts: []
+    }
+  },
+  methods: {
+    getData() {
+      this.$http.get('https://cnodejs.org/api/v1/topics?limit=20', {
+        limit: 5
+      }).then(res => {
+        this.posts = res.data.data
+      }).catch(function (err) {
+        console.log(err)
+      })
+    }
+  },
+  beforeMount() {
+    this.getData()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+img {
+  width: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.count{
+  margin: 0 20px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.title{
+  margin: 0 20px;
 }
-a {
-  color: #42b983;
+.tab{
+  color: red;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import Axios from 'axios'
 
+Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
+Vue.filter('tabFormatter',function (post){
+  if (post.good === true){
+    return '精华'
+  }else if(post.top === true){
+    return '置顶'
+  }else if(post.tab === 'ask'){
+    return  '问答'
+  }else if(post.tab === 'share'){
+    return '分享'
+  }else {
+    return '招聘'
+  }
+})
